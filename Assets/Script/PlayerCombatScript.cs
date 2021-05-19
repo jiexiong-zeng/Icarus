fe591d7dd266f9b43e752adaa1f99136326e9b8f
Jiexiong <jiexiong123@hotmail.com>
2021-05-19 18:24:44 +0800

    Knight Player Character Added

--- Assets/Script/PlayerCombatScript.cs
+++ Assets/Script/PlayerCombatScript.cs
@@ -16,48 +16,59 @@ public class PlayerCombatScript : MonoBehaviour
     public float attackRate = 2f;
     float nextAttackTime = 0f;
 
-    float attackDamageTime = 0f;
+    //float attackDamageTime = 0f;
     public float damagedelay = 0.25f;
-    bool attacked = false;
 
     // Update is called once per frame
     void Update()
     {
-
-
-        if (Time.time >= nextAttackTime)
+        if (Input.GetButtonDown("Attack"))
         {
-            Playermove.Attacking = false;
+            animator.SetBool("Attack1", true);
+        }
+
+    }
+        /*
+
+            if (Time.time >= nextAttackTime)
+        {
+            //Playermove.Attacking = false;
             if (Input.GetButtonDown("Attack"))
             {
-                animator.SetTrigger("Attack");
-                //Attack();
-                attacked = false;
-                Playermove.Attacking = true;
-                nextAttackTime = Time.time + 1f / attackRate;
-                attackDamageTime = Time.time + damagedelay;
+                animator.SetBool("Attack1", true);
+                //attacked = false;
+                //Playermove.Attacking = true;
+                //nextAttackTime = Time.time + 1f / attackRate;
+                //attackDamageTime = Time.time + damagedelay;
             }
         }
-        if (Time.time >= attackDamageTime && Playermove.Attacking == true && !attacked)
-        {
-            Attack();
-            attacked = true;
-        }
-    }
+        //if (Time.time >= attackDamageTime && Playermove.Attacking == true && !attacked)
+        //{
+        //    Attack();
+        //    attacked = true;
+        //}*/
+  
 
-    void Attack()
+    public void Attack(float delay, int attackDamage)
     {
-        //animator.SetTrigger("Attack");
-       
+        Playermove.Attacking = true;
+        StartCoroutine(DelayAction(delay));
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
 
         foreach(Collider2D enemy in hitEnemies)
         {
+            Debug.Log("We hit " + enemy.name);
             enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
         }
 
     }
 
+    IEnumerator DelayAction(float delayTime)
+    {
+        //Wait for the specified delay time before continuing.
+        yield return new WaitForSeconds(delayTime);
+        //Do the action after the delay time has finished.
+    }
 
     void OnDrawGizmosSelected()
     {
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -6,8 +6,10 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
     public Animator animator;
+    public Collider2D playerCollider;
     float horizontalMove = 0f;
     public float runSpeed = 40f;
+    public float dashSpeed = 100f;
     bool jump = false;
     bool crouch = false;
     public bool Attacking;
@@ -15,19 +17,30 @@ public class PlayerMovement : MonoBehaviour
 
     public float jumpbuffer = 0.2f;
     float jumptime;
+
+    bool dash = false;
+    float dashtime = 0f;
+    public float dashduration = 0.5f;
+    float dashDirection = 0f;
+    float invulnerableDuration = 0.2f;
+    float invulnerableTime = 0f;
+   
     // Update is called once per frame
     void Update()
     {
-        
-       
+        playerCollider.enabled = true;
+
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
         animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
         jumptime -= Time.deltaTime;
+        dashtime -= Time.deltaTime;
+        invulnerableTime -= Time.deltaTime;
 
         if (Input.GetButtonDown("Jump"))
         {
             jumptime = jumpbuffer;
-            animator.SetBool("Jumping", true);
+            animator.SetBool("Jumping",true);
         }
         if (jumptime > 0)
         {
@@ -38,30 +51,59 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonDown("Crouch"))
         {
             crouch = true;
-        } else if (Input.GetButtonUp("Crouch"))
+        } 
+        else if (Input.GetButtonUp("Crouch"))
         {
             crouch = false;
         }
 
+
+        if (Input.GetButtonDown("Dash"))
+        {
+            dashtime = dashduration;
+            invulnerableTime = invulnerableDuration;
+            dashDirection = Input.GetAxisRaw("Horizontal");
+        }
+        if (dashtime > 0)
+        {
+            dash = true;
+        }
+        if(invulnerableTime > 0)
+        {
+            playerCollider.enabled = false;
+        }
 
     }
     
     
     public void OnLanding ()
     {
-        animator.SetBool("Jumping", false);
+        //Debug.Log("Fell");
+        //animator.SetBool("Falling", false);
     }
 
     void FixedUpdate()
     {
-        
+        animator.SetBool("Roll", false);
+
         if (Attacking)
         {
-            controller.Move(0, false, false);
+            controller.Stop();
         }
-        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+
+        else if (dash)
+        {
+            controller.Move(dashDirection* dashSpeed* Time.fixedDeltaTime, false, false);
+            animator.SetBool("Roll", true);
+        }
+
+        else
+        {
+            controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        }
         
         jump = false;
+        dash = false;
         //crouch = false;
     }
 
